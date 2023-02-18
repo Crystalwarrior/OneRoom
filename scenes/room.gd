68bf5bc498535049b3d5f64b83b6772be2900b99
Alex Noir <Varsash@gmail.com>
2023-02-18 22:28:07 +0300

    Remove extra nodes (they blocked mouse input)

--- scenes/room.gd
+++ scenes/room.gd
@@ -4,8 +4,8 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var resource = preload("res://dialogues/start.dialogue")
+	# TODO: change this up and customize more to suit the game
 	DialogueManager.show_example_dialogue_balloon(resource, "this_is_a_node_title")
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
